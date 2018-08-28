f470acc9799402f9359d7360a3c48f7e8552239f
Andre Junqueira <andre@MacBook-Pro-de-Andre.local>
2018-08-28 15:10:55 -0300

    feat: Add functionality that returns bandeira

--- src/functions.js
+++ src/functions.js
@@ -30,18 +30,25 @@ export default (function(){
         return '(' + String(_patchPostMessageFunction) + ')();';
     }
 
-    generateHtmlCode = config => inputStyle => `
+    generateHtmlCode = config => (inputStyle, placeholder) => `
         <html>
             <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0"> 
             <script src="https://s3-sa-east-1.amazonaws.com/widgets.superlogica.net/embed.js" ></script>
             <script type="text/javascript">
                 superlogica.require("pjbank");
-                superlogica.pjbank("checkout_transparente","${config.credencial}");
-                function handleChange(e){window.postMessage(e)}
+                superlogica.pjbank("checkout_transparente","${config.credencial}","${config.homologacao}");
+                function handleChange(token){
+                    if(token){
+                        var bandeira = document.getElementById("bandeira").value;                    
+                        var data = {token: token, bandeira: bandeira}
+                        window.postMessage(JSON.stringify(data));
+                    }
+                }
             </script>
             <body style='margin: 0; padding: 0'>
-                <input type='number' class="pjbank-cartao" id="cartao" placeholder="${config.placeholder}" required style='width:100%; height: 100%; border-color: transparent; ${inputStyle}'>
+                <input type='number' class="pjbank-cartao" id="cartao" placeholder="${placeholder}" required style='width:100%; height: 100%; border-color: transparent; ${inputStyle}'>
                 <input type="hidden" name="pjbank-token" class="pjbank-token" onchange='handleChange(this.value)'>
+                <input type="hidden" id="bandeira" name="pjbank-cartao-bandeira" class="pjbank-cartao-bandeira">
             </body>
         </html>
     `;
